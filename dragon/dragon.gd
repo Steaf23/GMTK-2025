c75b4626ecedbb315a746fdd2885cc6bf4ff0cf9
Steaf23 <steafmobile@gmail.com>
2025-07-30 23:51:18 +0200

    - fixed most jittery movement
    - added for now unused speed multiplier

--- dragon/dragon.gd
+++ dragon/dragon.gd
@@ -2,6 +2,9 @@ class_name Dragon
 extends Node2D
 
 @export var follow_distance: int = 15
+@export var speed: int = 150
+@export var speed_mult: float = 1.0
+
 
 func _ready() -> void:
 	for i in 10:
@@ -10,26 +13,40 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	
+	var cur_speed = speed
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		cur_speed = speed * speed_mult
+	
+	$Head.speed = cur_speed
 	var leading: Node2D = $Head
+	var idx = 0
 	for b in $Body.get_children():
 		var target = leading.global_position.direction_to(b.global_position) * follow_distance + leading.global_position
 		
-		#if b is RigidBody2D:
-			#b.apply_central_impulse(b.global_position.direction_to(leading.global_position) * follow_distance + leading.global_position)
+		var direction = target - b.global_position
+		var distance = direction.length()
 		
-		b.velocity = b.global_position.direction_to(target) * 100
+		if distance < 1.0:
+			break
+	
+		var target_velocity = b.global_position.direction_to(target) * cur_speed
+		b.velocity = b.velocity.lerp(target_velocity, 0.4)
+		#b.velocity = lerp(old_velocity, b.velocity, 0.1)
 		b.move_and_slide()
 		leading = b
-
+		idx += 1
+		
 
 func add_segment() -> void:
 	var segment = preload("res://dragon/body_segment.tscn").instantiate()
 	
-	var back: Node2D = $Head
+	var leading: Node2D = $Head
 	if $Body.get_child_count() > 0:
-		back = $Body.get_child(-1)
+		leading = $Body.get_child(-1)
 	
-	segment.global_position = back.global_position
+	segment.global_position = leading.global_position
+	segment.add_collision_exception_with(leading)
+	leading.add_collision_exception_with(segment)
 	$Body.add_child(segment)
 
 
--- dragon/head.gd
+++ dragon/head.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 100
+var speed: int = 0
 
 func _physics_process(delta: float) -> void:
 	var dir = get_viewport_rect().get_center().direction_to(get_viewport().get_mouse_position())
