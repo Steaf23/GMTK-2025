bd9ceedcd756c7d94b4ca5b301bdce8a766c15e6
Steaf23 <steafmobile@gmail.com>
2025-08-03 04:01:47 +0200

    pause menu and volume settings (and sound manager)

---
+++ Util/SoundManager/sounds.gd
@@ -0,0 +1,3 @@
+extends Node
+
+const MUSIC_GAME: StringName = &"res://sound/game_music.wav"
---
+++ Util/pause_menu.gd
@@ -0,0 +1,33 @@
+extends VBoxContainer
+
+
+func _ready() -> void:
+	hide()
+	
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause") or (event.is_pressed() and event is InputEventKey and event.physical_keycode == KEY_ESCAPE):
+		toggle_pause()
+
+
+func toggle_pause() -> void:
+	if get_tree().paused:
+		unpause()
+	else:
+		pause()
+		
+
+func pause() -> void:
+	show()
+	SoundManager.muffle_music(true)
+	get_tree().paused = true
+	
+	
+func unpause() -> void:
+	hide()
+	SoundManager.muffle_music(false)
+	get_tree().paused = false
+
+
+func _on_resume_pressed() -> void:
+	toggle_pause()
